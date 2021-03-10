77b971f942ff741fbe650290506fedbe937e0a56
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-09 20:14:52 -0500

    adds dog create action

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,11 @@
 class Api::DogsController < ApplicationController
+  def create
+    @dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    @dog.save
+    render "show.json.jb"
+  end
 end
